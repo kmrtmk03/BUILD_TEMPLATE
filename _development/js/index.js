8729ab0f5214b51a03902ab4a04ab12ab9e26826
Kimura Tomoki <krmmmmmmrk@gmail.com>
2018-01-29 13:12:40 +0900

    タイムスタンプ変更？

--- _development/js/index.js
+++ _development/js/index.js
@@ -60,11 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 2:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -73,5 +74,6 @@
 console.log('INDEX!!!!');
 
 /***/ })
-/******/ ]);
+
+/******/ });
 //# sourceMappingURL=index.js.map
